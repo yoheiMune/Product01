6a5df4186a73c07fceff3d8a44ca466755691ec5
GS04-ID-nishijima <kurasshu@qc4.so-net.ne.jp>
2016-07-16 12:32:35 +0900

    初API「hostlist」、デバッグ前

---
+++ include/message.php
@@ -0,0 +1,12 @@
+<?php
+
+$msg_http_404_error001 = 'HTTP/1.1 404 Bad Request';
+$msg_http_500_error001 = 'HTTP/1.1 500 Internal Server Error';
+
+$msg_hostlistApi_parameter_error001 = 'no mode or withinOneWeekFlag...';
+$msg_hostlistApi_parameter_error002 = 'latitudo or longitude are not enough...';
+
+$msg_db_connect_error_001 = 'DB connect error...';
+
+
+?>
